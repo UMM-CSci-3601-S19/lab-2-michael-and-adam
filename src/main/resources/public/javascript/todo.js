9e3ca209e77bbd98465c704edac6521eca021e91
michael-small <small203@morris.umn.edu>
2019-02-14 20:53:31 -0600

    #5 Starts the process of retrieving owner Blanche

---
+++ src/main/resources/public/javascript/todo.js
@@ -0,0 +1,41 @@
+function getAllTodos() {
+  console.log("Getting all owners.");
+
+  var HttpThingy = new HttpClient();
+  HttpThingy.get("/api/todos", function (returned_json) {
+    document.getElementById('jsonDump').innerHTML = returned_json;
+  });
+}
+//borrowed from users.js
+/**
+ * Wrapper to make generating http requests easier. Should maybe be moved
+ * somewhere else in the future!.
+ *
+ * Based on: http://stackoverflow.com/a/22076667
+ * Now with more comments!
+ */
+function HttpClient() {
+  // We'll take a URL string, and a callback function.
+  this.get = function (aUrl, aCallback) {
+    var anHttpRequest = new XMLHttpRequest();
+
+    // Set a callback to be called when the ready state of our request changes.
+    anHttpRequest.onreadystatechange = function () {
+
+      /**
+       * Only call our 'aCallback' function if the ready state is 'DONE' and
+       * the request status is 200 ('OK')
+       *
+       * See https://httpstatuses.com/ for HTTP status codes
+       * See https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/readyState
+       *  for XMLHttpRequest ready state documentation.
+       *
+       */
+      if (anHttpRequest.readyState === 4 && anHttpRequest.status === 200)
+        aCallback(anHttpRequest.responseText);
+    };
+
+    anHttpRequest.open("GET", aUrl, true);
+    anHttpRequest.send(null);
+  }
+}
