0415dbdebcbfc247e558f1a8d17813a16e5c9adb
michael-small <small203@morris.umn.edu>
2019-02-17 16:14:04 -0600

    Started the status epic

--- src/main/resources/public/javascript/todo.js
+++ src/main/resources/public/javascript/todo.js
@@ -59,6 +59,15 @@ function getAllTodosByCategory() {
   });
 }
 
+function getTodoStatus() {
+  console.log("Getting all the todos by status.");
+
+  var HttpThingy = new HttpClient();
+  HttpThingy.get("/api/todos?status=" + document.getElementById("status1").value, function (returned_json) {
+    document.getElementById('jsonDump').innerHTML = returned_json;
+  });
+}
+
 /**
  * Wrapper to make generating http requests easier. Should maybe be moved
  * somewhere else in the future!.
